d89667091101076542ba48ce83505b11d049d73c
Rens van de schoot <36502709+Rensvandeschoot@users.noreply.github.com>
2022-07-06 15:37:08 +0200

    Add old files for v2.0 (#5)

--- ui.R
+++ ui.R
@@ -1,100 +1,117 @@
+# --------------------------------------------------------
+# FBI - First Bayesian Inference
+# Lion Behrens, Sonja D. Winter and Rens van de Schoot
+# February 2018
+# User Interface
+# --------------------------------------------------------
 
-ui <- fluidPage(title="Exploring Bayesian Estimation",
-#######################################################
-  fluidRow(
-    column(12, style="background:  #6699ff;
-        padding: 20px; border-width: 20px; border-color: #fff;
-           border-style: solid;",
-           h1("Exploring Bayesian Estimation (Version 1.1)", span(style = "font-weight: 300"),
-              style="color: #fff; text-align: center;"),
-           p("Created by: Sonja D. Winter, Lion Behrens & Rens van de Schoot, Utrecht University",
-             style="color: #fff; text-align: center;")
-           )
-  ),
- 
-  
-  fluidRow(
-    column(4,
-           h2("1. Choose a prior distribution"),
-           br(),
-           radioButtons(inputId = "prior", label = "Distributions",
-                        choices = list("Normal" = "rnorm", "Uniform" = "runif",
+ui <- fluidPage(# Layout
+                theme="simplex.min.css",
+                tags$style(type="text/css", "label {font-size: 12px;}", ".recalculating {opacity: 1.0;}"),
+                tags$hr(style="border-color: blue;"),
+                
+                # Application title and links
+                tags$h2("FBI: First Bayesian Inference ", img(src='utrecht.png', align = "right", height=112, width=405)),
+                p("Version 2.0, created by", tags$a(href="https://lion-be.github.io/", "Lion Behrens,"), tags$a(href="https://thequantitativepsyche.wordpress.com/", "Sonja D. Winter"), "and", 
+                  tags$a(href="https://www.rensvandeschoot.com/", "Rens van de Schoot")), 
+                actionButton("disclaimer", label = "Show Disclaimer"),
+                br(),  
+                br(),      
+                p("This Shiny-app was designed to aid in teaching the basics of Bayesian estimation.
+                  The focus of the analysis presented here is on accurately estimating the mean of IQ 
+                  using simulated data. This implies that priors and data should be generated within the theoretical boundaries 
+                  of an imaginary IQ test with a minimum and maximum possible scores of 40-180. 
+                  Specifying priors and/or generating data outside these limits might cause the app to return 
+                  with unwanted solutions. For more details see..."),
+                p("Van de Schoot, R., Kaplan, D., Denissen, J., Asendorpf, J. B., Neyer, F. J., & Aken, M. A. (2014).", tags$a(href="https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4158865/", "A gentle introduction to Bayesian analysis: applications to developmental research. "),
+                  "Child development, 85(3), 842-860."),
+                tags$hr(style="border-color: blue;"),
+                
+fluidRow(
+    column(4,tags$h3("1. Choose a prior distribution"),
+           p("Choose the parameters of your prior distribution. Hit the button below to create your prior."),
+           wellPanel(
+      
+           radioButtons(inputId = "prior", label = "Prior Distributions",
+                        choices = list("Uniform" = "runif",
                                        "Truncated Normal" = "trnorm")),
-           conditionalPanel(
-             condition = "input.prior == 'rnorm'",
-             numericInput("mean", label = "Mean", value = 100),
-             numericInput("var", label = "Variance", value = 10)
-           ),
-           conditionalPanel(
+          conditionalPanel(
              condition = "input.prior == 'runif'",
-             numericInput("min", label = "Minimum", value = 0),
-             numericInput("max", label = "Maximum", value = 150)
+             numericInput("min", label = "Minimum", value = 40),
+             numericInput("max", label = "Maximum", value = 180)
            ),
            conditionalPanel(
              condition = "input.prior == 'trnorm'",
-             numericInput("meant", label = "Mean", value = 100),
-             numericInput("vart", label = "Variance", value = 10),
-             numericInput("lbound", label = "Lower bound", value = 0),
-             numericInput("ubound", label = "Higher bound", value = 150)
-           ),
+             numericInput("meant", label = "Prior Mean", value = 90),
+             numericInput("vart", label = "Prior Variance", value = 10),
+             numericInput("lbound", label = "Lower bound", value = 40),
+             numericInput("ubound", label = "Higher bound", value = 180)
+           ),  
            
-           br(),
-   
-           h2("2a. Upload your data"),
-           p("Please submit data in .csv format with only one column of data
-             and the variable name as the header."),
-           fileInput(inputId="file", label = h3("Upload"),
-                     accept = c(
-                       'text/csv',
-                       'text/comma-separated-values',
-                       'text/tab-separated-values',
-                       'text/plain',
-                       '.csv',
-                       '.tsv'
-                     ))),
+           actionButton("construct_prior", label = "Construct Prior")
            
+    )),
+  
+    
+    column(4, tags$h3("2. Construct your data and likelihood"),
+            p("You can simulate data from a truncated normal distribution with 40 and 180 as boundary values. From this data, a 
+              likelihood function will be constructed."),  
+            
+            wellPanel(
+          
+              strong("Parameters of your simulated data"),
+              
+              br(),
+              br(),
+              
+              numericInput("meant2", label = "Data Mean", value = 100),
+              numericInput("vart2", label = "Data Standard Deviation", value = 15),
+              numericInput("n2", label = "Sample Size", value = 22),
+              
+              br(),
+              
+              strong("This will lead to the following parameters of the likelihood function"), 
+              br(),
+              
+              p(textOutput("likeli_mean")),
+              p(textOutput("likeli_var")),
+              
+              br(),
+              
+              actionButton("generate", label = "Construct Dataset and Likelihood")
+            
+    )),
+    
+    column(4, tags$h3("3. Find your posterior"),
            
-           
-      column(4,     
-           h2("2b. Generate your data"), 
-           p("By specifying the parameters below, you can generate data from a truncated
-             normal distribution."),
-           br(),
-           numericInput("meant2", label = "Mean", value = 100),
-           numericInput("vart2", label = "Variance", value = 10),
-           numericInput("lbound2", label = "Lower bound", value = 40),
-           numericInput("ubound2", label = "Higher bound", value = 180), 
-           numericInput("n2", label = "Sample Size", value = 50),
-           
-           actionButton("generate", label = "Generate Data")
-           
-           
-    ),
-    column(4,
-           h2("3. Find your posterior!"),
-           br(),
-           p("By clicking the button below, you run the model to find the
-             posterior mean of your data with your uploaded data and chosen
-             prior distribution. If you change your data or prior, and you
+           p("Hit the button to run the model to find the
+             posterior mean of based on your uploaded data and chosen
+             prior distribution."),
+           wellPanel(
+             actionButton("run", label = "Construct Posterior (default)"),
+             br(),
+             br(),
+             actionButton("runsigmaunknown", label = "Run with sigma unknown"),
+             br(),
+             br(),
+           p("If you change your data or prior, and you
              want to see its effect, just rerun the model by clicking the
-             button again!"),
-           actionButton("run", label = "Run the model"))
-    ),
+             button again.")
+             )
+    )),
+
+
   fluidRow(
     column(12, 
-           h2("Distributions"),
+           h2("Plot"),
            br(),
            plotOutput("hist")
-           )
-  ),
+  )),
+
+
   fluidRow(
-    column(12, style="background:  #6699ff;
-        padding: 0px 200px 0px 200px",
-           #\href{http://onlinelibrary.wiley.com/doi/10.1111/cdev.12169/abstract}{}
-            p("Based on: van de Schoot, R., Kaplan, D., Denissen, J., Asendorpf, J. B., 
-             Neyer, F. J., & Aken, M. A. (2014). A gentle introduction to Bayesian analysis: applications to developmental research. Child development, 85(3), 
-             842-860.", style="color: #fff; text-align: center; padding: 20px"
-              )
-           
-    )
+    column(12,
+           h2("Summary"),
+           br(),
+           tableOutput("tbl"))
   ))
